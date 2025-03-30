78fb9ba9fd36064be7f64e6183db6944b6b4b378
vincent-mugendi <vincentmwathi5@gmail.com>
2025-03-30 21:56:33 +0300

    Add State Management

--- frontend/src/components/client-dashboard.jsx
+++ frontend/src/components/client-dashboard.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 export default function HakikaDashboard() {
+  const [products, setProducts] = useState([]);
+  const [productName, setProductName] = useState("");
+  const [batchNumber, setBatchNumber] = useState("");
+  const [csvFile, setCsvFile] = useState(null);
+
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Hakika Client Dashboard</h1>
+      <h1 className="text-2xl font-bold">Hakika Business Dashboard</h1>
     </div>
   );
 }
