df769c7e1ba058c4c288b18bab040c72e8495e18
vincent-mugendi <vincentmwathi5@gmail.com>
2025-04-01 19:59:46 +0300

    Defining routes properly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import HakikaClientDashboard from "./components/HakikaClientDashboard" 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HakikaClientDashboard from "./components/HakikaClientDashboard";
+import VerifyProduct from "./pages/QRVerificationPage"; // Adjust if the path is different
 
 function App() {
-
   return (
-    <>
-      <HakikaClientDashboard />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HakikaClientDashboard />} />
+        <Route path="/verify" element={<VerifyProduct />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
