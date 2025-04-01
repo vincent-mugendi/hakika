8e506e77d19761883cccb507b6a320441f8a52bb
vincent-mugendi <vincentmwathi5@gmail.com>
2025-04-01 20:29:04 +0300

    Updating dependancies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HakikaClientDashboard from "./components/HakikaClientDashboard";
-import VerifyProduct from "./pages/QRVerificationPage"; // Adjust if the path is different
+import VerifyProduct from "./pages/QRVerificationPage";
 
 function App() {
   return (
